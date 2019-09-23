ac5096a3f8504c06545b7077c9e0b15afc96f70d
Student <example@example.com>
2019-09-23 09:13:29 -0700

    Add for loop and condition to return 'ping', 'pong' 'ping-pong' or number.

--- PingPong.cs
+++ PingPong.cs
@@ -4,6 +4,28 @@ class PingPong
 {
     static void Main()
     {
-        
+        Console.WriteLine("Please enter a number.");
+        string userInput = Console.ReadLine();
+        int inputNumber = int.Parse(userInput);
+
+        for (int i = 1; i < inputNumber; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("ping-pong");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("pong");
+            }
+            else if (i % 3 == 0 )
+            {
+                Console.WriteLine("ping");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
