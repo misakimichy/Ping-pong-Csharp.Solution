6e6810237d65a36d5626239eb6ea980b62963817
Misaki Koonce <misaki.koonce@gmail.com>
2019-12-18 16:13:06 -0800

    Add method to check if the user input is a number. Return error message if it's not a number.

--- PingPong/Program.cs
+++ PingPong/Program.cs
@@ -8,13 +8,29 @@ namespace MainProgram
     {
         public static void Main()
         {
-            Console.WriteLine("Please enter a number.");
-            int inputNumber = int.Parse(Console.ReadLine());
-            PingPong userPong = new PingPong();
-            List<string> result = userPong.IsPingPong(inputNumber);
-            foreach(string current in result)
+            Console.WriteLine("Enter a number and we return:\n Returns 'ping' if it can be divided by 3,\n Returns 'pong' if it can be divided by 5,\n Returns 'ping-pong' if it can be divied by 3 and 5.\n Returns the number if the number doesn't meet these conditions.\n");
+            StartPingPong();
+        }
+
+        private static void StartPingPong()
+        {
+            Console.Write("Please enter a number: ");
+            int userInput;
+            bool success = Int32.TryParse(Console.ReadLine(), out userInput);
+            if(success)
             {
-                Console.WriteLine(current);
+                // PingPong userPong = new PingPong();
+                List<string> result = PingPong.IsPingPong(userInput);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(string.Join(", ", result));
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid Input");
+                Console.ResetColor();
+                StartPingPong();
             }
         }
     }
